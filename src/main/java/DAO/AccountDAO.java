fe5d7438a712b69fcacde9197155dd111fa266e3
Elijah Monzon <eli.develops@gmail.com>
2025-06-13 21:21:01 +0000

    added db account creation and retrieval functions

--- src/main/java/DAO/AccountDAO.java
+++ src/main/java/DAO/AccountDAO.java
@@ -1,5 +1,80 @@
 package DAO;
 
+import java.sql.*;
+import java.util.*;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
 public class AccountDAO {
     
+    public List<Account> getAllAccounts(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Account> accounts = new ArrayList<>();
+        try {
+            //Write SQL logic here
+            String sql = "SELECT * FROM account;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Account Account = new Account(rs.getInt("account_id"), rs.getString("username"), rs.getString("password"));
+                accounts.add(Account);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return accounts;
+    }
+
+    public Account insertAccount(Account acc){
+        if(acc.getUsername().isBlank() || acc.getPassword().length() < 4 || getAccountByUsername(acc.getUsername()) != null){
+            return null; //invalid username, invalid password, username already exists: return null
+        }
+
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+//          Write SQL logic here. You should only be inserting with the name column, so that the database may
+//          automatically generate a primary key.
+
+            String sql = "INSERT INTO account (username, password) VALUES (?,?);" ;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            preparedStatement.setString(1, acc.getUsername());
+            preparedStatement.setString(2, acc.getUsername());
+
+            
+            preparedStatement.executeUpdate();
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+            if(pkeyResultSet.next()){
+                int generated_Account_id = (int) pkeyResultSet.getLong(1);
+                return new Account(generated_Account_id, acc.getUsername(), acc.getPassword());
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Account getAccountByUsername(String username){
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            //Write SQL logic here
+            String sql = "SELECT * FROM account WHERE username = ?;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            //write preparedStatement's setInt method here.
+            preparedStatement.setString(1, username);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Account acc = new Account(rs.getInt("account_id"),
+                        rs.getString("username"),
+                        rs.getString("password"));
+                return acc;
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
